52cd751b6b52ecd05e002296075a3e24467948dc
Yoona jeong <yoonalove77@naver.com>
2022-05-12 14:09:17 +0900

    Add build Folder

--- client/src/index.js
+++ client/src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
             window.__REDUX_DEVTOOLS_EXTENSION__()
         )}
     >
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
